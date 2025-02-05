0a21e2009d45a44c31870ab5a5d813b032572cc6
barbute <jackhdemeterio@gmail.com>
2025-02-04 18:10:25 -0600

    [JD] Initial commit of production
    Brought files from production in template-talon_standard to here

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,100 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+
+
+public class Robot extends LoggedRobot {
+    private Command mAutonomousCommand;
+    private Command mTeleopCommand;
+
+    private RobotContainer mRobotContainer;
+
+    // ==================== Robot Power On ====================
+    @Override
+    public void robotInit() {
+        // Set up Logging
+        Logger.addDataReceiver(new NT4Publisher());
+
+        Logger.start();
+
+        mRobotContainer = new RobotContainer();
+    }
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+    // ==================== Disabled ====================
+    @Override
+    public void disabledInit() {
+    }
+
+    @Override
+    public void disabledPeriodic() {
+    }
+
+    @Override
+    public void disabledExit() {
+    }
+
+    // ==================== Autonomous ====================
+    @Override
+    public void autonomousInit() {
+        mAutonomousCommand = mRobotContainer.getAutonomousCommand();
+
+        if (mAutonomousCommand != null) {
+            mAutonomousCommand.schedule();
+        }
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void autonomousExit() {
+    }
+
+    // ==================== Teleop ====================
+    @Override
+    public void teleopInit() {
+        if (mAutonomousCommand != null) {
+            mAutonomousCommand.cancel();
+        }
+        mTeleopCommand = mRobotContainer.getTeleopCommand();
+        if (mTeleopCommand != null) {
+            mTeleopCommand.schedule();
+        }
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void teleopExit() {
+    }
+
+    // ==================== Test ====================
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void testExit() {
+    }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,90 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class RobotContainer {
+
+    private final CommandXboxController driverController = new CommandXboxController(0);
+    private final CommandXboxController operatorController = new CommandXboxController(1);
+
+    // Define subsystems
+    // ex: private final LEDSubsystem LEDs;
+
+    // Define other utility classes
+    private final AutonCommands autonCommands;
+    private final TeleopCommands telopCommands;
+
+    private LoggedDashboardChooser<Command> autoChooser;
+
+    private final boolean useCompetitionBindings = true;
+
+    public RobotContainer() {
+
+        // If using AdvantageKit, perform mode-specific instantiation of subsystems.
+        switch (Constants.kCurrentMode) {
+            case REAL:
+                // Instantiate subsystems that operate actual hardware (Hardware controller based modules)
+                break;
+            case SIM:
+                // Instantiate subsystems that simulate actual hardware (IOSim modules)
+                break;
+            default:
+                // Instantiate subsystems that are driven by playback of recorded sessions. (IO modules)
+                break;
+        }
+
+        // Instantiate subsystems that don't care about mode, or are non-AdvantageKit enabled.
+        // ex: LEDs = new LEDSubsystem();
+
+        // Instantiate your TeleopCommands and AutonCommands classes
+        telopCommands = new TeleopCommands(/* pass subsystems here */);
+        autonCommands = new AutonCommands(/* pass subsystems here */);
+        try {
+            autoChooser = new LoggedDashboardChooser<>("Auton Program", autonCommands.getAutoChooser());
+            // Fill instant command with whatever your initial action is
+            autoChooser.addDefaultOption("initActionZeroPath", new InstantCommand());
+        } catch (Exception e) {
+            autoChooser = new LoggedDashboardChooser<Command>("Auton Program");
+            // Fill instant command with whatever your initial action is, to prepare for the case of failure
+            autoChooser.addDefaultOption("initActionZeroPath", new InstantCommand());
+        }
+
+
+        // Pass subsystems to classes that need them for configuration
+
+
+        // Create any Dashboard choosers (LoggedDashboardChooser, etc)
+
+        // Configure controls (drivebase suppliers, DriverStation triggers, Button and other Controller bindings)
+
+        configureStateTriggers();
+        configureButtonBindings();
+    }
+
+    public Command getTeleopCommand() {
+        return new SequentialCommandGroup(
+            // Commands to run on teleop go here.
+        );
+    }
+
+    public Command getAutonomousCommand() {
+        return autoChooser.get();
+    }
+
+    private void configureStateTriggers() {
+
+
+    }
+
+    private void configureButtonBindings() {
+
+    }
+}
