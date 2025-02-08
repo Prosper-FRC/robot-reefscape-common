cc3467566ae7942f05747dae6e8d4763dd5c906f
barbute <jackhdemeterio@gmail.com>
2025-02-08 12:04:11 -0600

    Clean up SensorIOLaserCAN.java
    Remove dead code and imports
    
    Add logic to check if there's a valid measurement, and if so mark the sensor as connected
    
    TODO Test to see if this actually works on the code

--- src/main/java/frc/robot/subsystems/intake/SensorIOLaserCAN.java
+++ src/main/java/frc/robot/subsystems/intake/SensorIOLaserCAN.java
@@ -4,63 +4,33 @@
 
 package frc.robot.subsystems.intake;
 
-import static edu.wpi.first.units.Units.Millimeters;
-
-import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.configs.CANrangeConfiguration;
 import au.grapplerobotics.LaserCan;
-import au.grapplerobotics.interfaces.LaserCanInterface.Measurement;
-import edu.wpi.first.units.DistanceUnit;
-import edu.wpi.first.units.measure.Distance;
+import au.grapplerobotics.interfaces.LaserCanInterface;
 import au.grapplerobotics.ConfigurationFailedException;
 
 /** 
- * A class to interact with the CANRange sensor 
+ * A class to interact with the LaserCAN sensor 
  * 
  * Related documentation:
- * https://v6.docs.ctr-electronics.com/en/stable/docs/hardware-reference/canrange/index.html
- * https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/hardware/CANrange.html
+ * https://grapplerobotics.au/product/lasercan/
  */
 public class SensorIOLaserCAN implements SensorIO {
   private final LaserCan kSensor = new LaserCan(IntakeConstants.kCANrangeID);
-  
-
-  private StatusSignal<Boolean> detectsObject;
 
   public SensorIOLaserCAN() {
-    try
-    {  
+    try {  
       kSensor.setRangingMode(LaserCan.RangingMode.SHORT);
       kSensor.setRegionOfInterest(new LaserCan.RegionOfInterest(8,8,16,16));
-    }
-    catch(ConfigurationFailedException e)
-    {
+    } catch(ConfigurationFailedException e) {
       System.out.println("Configuration Failed! " +e);
     }
-
-
-    // kRangeConfiguration.ProximityParams.ProximityThreshold = 
-    //   IntakeConstants.kSensorConfiguration.kDetectionThresholdMeters();
-
-    // kSensor.getConfigurator().apply(kRangeConfiguration);
-
-    // detectsObject = kSensor.getIsDetected();
-
-    // BaseStatusSignal.setUpdateFrequencyForAll(IntakeConstants.kStatusSignalUpdateFrequencyHz, 
-    //   detectsObject);
-
-    // // Optimize the CANBus utilization by explicitly telling all CAN signals we
-    // // are not using to simply not be sent over the CANBus
-    // kSensor.optimizeBusUtilization(0.0, 1.0);
   }
 
   @Override
   public void updateInputs(SensorIOInputs inputs) {
-    // inputs.isConnected = BaseStatusSignal.refreshAll(detectsObject).isOK();
-    inputs.isConnected = true;
+    inputs.isConnected = kSensor.getMeasurement().status == LaserCanInterface.LASERCAN_STATUS_VALID_MEASUREMENT;
 
-    if (kSensor.getMeasurement() != null) {
+    if (kSensor.getMeasurement() != null && inputs.isConnected) {
       inputs.detectsObject = kSensor.getMeasurement().distance_mm < 5;
     } else {
       inputs.detectsObject = false;
