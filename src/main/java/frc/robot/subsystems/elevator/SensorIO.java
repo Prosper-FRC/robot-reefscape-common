48c10e1995755c667c9bdf2388e3bbfb5c7315e8
barbute <jackhdemeterio@gmail.com>
2025-02-10 17:23:19 -0600

    Add elevator sensor interface
    This will be used to interact with a DigitalInput device, which will be used to home the elevator to a 0'd point

---
+++ src/main/java/frc/robot/subsystems/elevator/SensorIO.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.elevator;
+
+import org.littletonrobotics.junction.AutoLog;
+
+/** Hardware interface for a digital sensor - used to "home" the elevator */
+public interface SensorIO {
+  @AutoLog
+  public static class SensorIOInputs {
+    public boolean isConnected = false;
+
+    /** 
+     * Boolean to hold whether or not the digital input device has been "activated"
+     * See these docs:
+     * https://docs.wpilib.org/en/stable/docs/software/hardware-apis/sensors/digital-inputs-software.html#digital-inputs-software
+     */
+    public boolean isActivated = false;
+  }
+
+  /**
+   * Write data from the hardware to the inputs object
+   * 
+   * @param inputs The inputs object
+   */
+  public default void updateInputs(SensorIOInputs inputs) {}
+}
