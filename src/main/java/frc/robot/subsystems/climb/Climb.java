394382e9a3d6f59d3e4adcaa061ec0270891db27
barbute <jackhdemeterio@gmail.com>
2025-02-13 18:36:19 -0600

    Add setters and getters for Climb

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -9,6 +9,7 @@ import java.util.function.DoubleSupplier;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.debugging.LoggedTunableNumber;
@@ -38,7 +39,7 @@ public class Climb extends SubsystemBase {
   private final ClimbIO[] kHardware;
   private final ClimbIOInputsAutoLogged[] kInputs;
 
-  /** Creates a new Climb. */
+  /** Creates a new Climb. Note that the first argument is considered the lead motor */
   public Climb(ClimbIO... io) {
     kHardware = new ClimbIO[io.length];
     kInputs = new ClimbIOInputsAutoLogged[io.length];
@@ -57,13 +58,65 @@ public class Climb extends SubsystemBase {
     }
 
     if (!DriverStation.isEnabled()) {
-      // stop
+      stop();
     }
 
     if (voltageGoal != null) {
-      // set voltage
+      setVoltage(voltageGoal.getGoalVoltage());
     }
 
     // Add position checking here 
+  }
+
+  /**
+   * Sets the voltage of the motor. Assumes that positive voltage moves the motor closer to the
+   * maximum possible physical position the mechanism can be in, while negative voltage moves 
+   * the motor closer to the minimum possible physical position the mechanism can be in
+   * 
+   * @param voltage
+   */
+  public void setVoltage(double voltage) {
+    // Notice how we are not checking if position control is running, it is up to the caller
+    // to check for this before calling this method (I recommend calling this subsystem's stop() 
+    // method after completing any action)
+    if (getPosition().getDegrees() > ClimbConstants.kMaxPosition.getDegrees() 
+        && voltage > 0) {
+      return;
+    } else if (getPosition().getDegrees() < ClimbConstants.kMinPosition.getDegrees()
+        && voltage < 0) {
+      return;
+    } else {
+      for (ClimbIO io : kHardware) {
+        io.setVoltage(voltage);
+      }
+    }
+  }
+
+  /** Stops the mechanism */
+  public void stop() {
+    voltageGoal = null;
+
+    for (ClimbIO io : kHardware) {
+      io.stop();
+    }
+  }
+
+  /** Resets all motor encoders to 0 */
+  public void resetPosition() {
+    for (ClimbIO io : kHardware) {
+      io.resetPosition();
+    }
+  }
+
+  /**
+   * Gets the position of the mechanism. Note that this assumes the caller only
+   * desires the position of the mechanism as a whole, not the position of the 
+   * individual motor
+   * 
+   * @return The position of the mechanism
+   */
+  public Rotation2d getPosition() {
+    // Only need to return one position since the motors are mechaically linked
+    return kInputs[0].position;
   }
 }
