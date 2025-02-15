e6f414cb3e9cf090e67f2dd6205fb9a6d5a57a0c
AnshulBompada <ancient.flame1145@gmail.com>
2025-02-15 09:57:59 -0600

    Tested all game states

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -19,7 +19,7 @@ public class VisionConstants {
         new Rotation3d(0.0, Math.toRadians(8.317), Math.toRadians(-25.0))
     );
 
-    public static final String kRightCamName = "5411-RIGHT";
+    public static final String kRightCamName = "5411_RIGHT";
     public static final Transform3d kRightCamTransform = new Transform3d(
         new Translation3d(0.3, -0.3, 0.0),
         // Accounts for cameras being on back
