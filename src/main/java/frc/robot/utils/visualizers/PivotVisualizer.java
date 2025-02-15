60c5f3ec1db179e9fd5991353174417abb02914e
barbute <jackhdemeterio@gmail.com>
2025-02-14 22:46:55 -0600

    Create pivot visualizer file

---
+++ src/main/java/frc/robot/utils/visualizers/PivotVisualizer.java
@@ -0,0 +1,11 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.utils.visualizers;
+
+// How to use the visualizer with AdvantageScope:
+// https://docs.advantagescope.org/tab-reference/mechanism
+
+/** Class to handle graphical visualization of a pivot (arm) mechanism. */
+public class PivotVisualizer {}
