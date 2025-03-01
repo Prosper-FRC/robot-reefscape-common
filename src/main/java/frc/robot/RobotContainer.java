d795fcebc118eec799eac7b662b35bb8bc7ac350
Aryan Kammili <aryan.kammili@gmail.com>
2025-03-01 11:26:27 -0600

    merge in goal pose chooser
    pls work

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -286,27 +286,21 @@ public class RobotContainer {
         new Trigger(DriverStation::isEnabled)
             .onTrue(
                 Commands.runOnce(() -> 
-                led.setGradientAnimation(
-                    100,
-                    GradientType.kContinuous,
-                    Color.kLightBlue,
-                    Color.kMediumBlue,
-                    Color.kDarkBlue)));
+                led.setBreatheAnimation(
+                    3.0,
+                    Color.kRed)));
 
         new Trigger(intake::detectedGamepiece)
         .whileTrue(
             Commands.runOnce(() -> 
                 led.setSolidBlinkAnimation(
-                0.1, 
+                0.2, 
                 Color.kRed)))
         .whileFalse(
             Commands.runOnce(() -> 
-            led.setGradientAnimation(
-                100, 
-                GradientType.kContinuous, 
-                Color.kLightBlue, 
-                Color.kMediumBlue, 
-                Color.kDarkBlue)));
+            led.setBreatheAnimation(
+                3.0,
+                Color.kRed)));
         
     }
 
@@ -370,10 +364,10 @@ public class RobotContainer {
                 .onTrue(robotDrive.setDriveStateCommandContinued(DriveState.INTAKE_HEADING_ALIGN))
                 .onFalse(robotDrive.setDriveStateCommand(DriveState.TELEOP));
 
-            driverController.leftBumper()
+            operatorController.button(kLeftAlign)
                 .onTrue(GoalPoseChooser.setSideCommand(SIDE.LEFT));
 
-            driverController.rightBumper()
+            operatorController.button(kRightAlign)
                 .onTrue(GoalPoseChooser.setSideCommand(SIDE.RIGHT));
 
             //TEMPORARY SCORE
@@ -565,6 +559,9 @@ public class RobotContainer {
             //     .whileFalse(
             //         teleopCommands.stopRollersAndPivotCommand()
             //     );
+
+            operatorController.leftStick()
+                .onTrue(Commands.runOnce(() -> elevator.resetPosition(), elevator));
 
             operatorController.rightStick()
                 .onTrue(
