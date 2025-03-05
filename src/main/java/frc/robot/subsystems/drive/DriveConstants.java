18dad023237258b04a48a5ceb916c027351143c4
Aryan Kammili <aryan.kammili@gmail.com>
2025-03-04 20:53:08 -0600

    Reset offsets becucase of stupid cancoders

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -82,28 +82,28 @@ public class DriveConstants {
             11, 
             21, 
             31,
-            Rotation2d.fromRotations(0.405029));
+            Rotation2d.fromRotations(0.406494));
 
     public static final ModuleHardwareConfig kFrontRightHardware =
         new ModuleHardwareConfig(
             12, 
             22, 
             32, 
-            Rotation2d.fromRotations(0.078857));
+            Rotation2d.fromRotations(0.079346));
 
     public static final ModuleHardwareConfig kBackLeftHardware =
         new ModuleHardwareConfig(
             13, 
             23, 
             33,
-            Rotation2d.fromRotations(0.006592));
+            Rotation2d.fromRotations(0.015625));
 
     public static final ModuleHardwareConfig kBackRightHardware =
         new ModuleHardwareConfig(
             14, 
             24, 
             34,
-            Rotation2d.fromRotations(-0.469238));
+            Rotation2d.fromRotations(-0.497314));
 
     public static record ModuleHardwareConfig(
         int driveID, int azimuthID, int encoderID, Rotation2d offset) {}
