0be3685836faff9227b7c2901d9acce0e4eb8f4d
AnshulBompada <ancient.flame1145@gmail.com>
2025-03-08 10:54:39 -0600

    Update at 'Sat Mar 08 10:54:39 CST 2025'

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -28,6 +28,6 @@ public class FieldConstants {
     public static final Pose2d EM = new Pose2d(5.13, 2.90, Rotation2d.fromDegrees(-60.0));
     public static final Pose2d ER = new Pose2d(5.05, 2.88, Rotation2d.fromDegrees(-60.0));
 
-    public static final Pose2d IL = new Pose2d(1.632, 7.42, Rotation2d.fromRadians(2.20).plus(Rotation2d.k180deg));
-    public static final Pose2d IR = new Pose2d(1.31, 0.95, Rotation2d.fromRadians(-2.20).plus(Rotation2d.k180deg));
+    public static final Pose2d IL = new Pose2d(1.632, 7.42, Rotation2d.fromRadians(2.20).plus(Rotation2d.k180deg).plus(Rotation2d.k180deg));
+    public static final Pose2d IR = new Pose2d(1.31, 0.95, Rotation2d.fromRadians(-2.20).plus(Rotation2d.k180deg).plus(Rotation2d.k180deg));
 }
