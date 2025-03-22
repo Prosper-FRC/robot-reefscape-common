029efd62bc2174e0c59b3a3bc0aaf46ba1dea82d
Alexander Phillips <86499013+AJPhillips005@users.noreply.github.com>
2025-03-22 09:17:26 -0500

    Fixed naming error

--- src/main/java/frc/robot/subsystems/drive/controllers/GoalPoseChooser.java
+++ src/main/java/frc/robot/subsystems/drive/controllers/GoalPoseChooser.java
@@ -98,8 +98,8 @@ public class GoalPoseChooser {
 
     public static Pose2d getIntakePose(Pose2d robotPose) {
         if(DriverStation.getAlliance().get().equals(Alliance.Blue)) {
-            return AllianceFlipUtil.apply((robotPose.getY() < Constants.kFieldWidthMeters / 2.0) ? FieldConstants.blueIR : FieldConstants.blueIL);
-        } else return (robotPose.getY() < Constants.kFieldWidthMeters / 2.0) ? FieldConstants.redIL : FieldConstants.redIR;
+            return AllianceFlipUtil.apply((robotPose.getY() < Constants.kFieldWidthMeters / 2.0) ? FieldConstants.B_IR : FieldConstants.B_IL);
+        } else return (robotPose.getY() < Constants.kFieldWidthMeters / 2.0) ? FieldConstants.R_IL : FieldConstants.R_IR;
     }
 
     /* DO NOT USE X COORDINATE, REPLACE y holonomic speeds with driver controller when using this! */
