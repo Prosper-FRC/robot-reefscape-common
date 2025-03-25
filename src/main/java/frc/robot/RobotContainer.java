5f53c81caa924f20dca79ced0dfd31e7e5ba02d5
Alexander Phillips <86499013+AJPhillips005@users.noreply.github.com>
2025-03-25 17:17:22 -0500

    MP - Correct button bindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -347,8 +347,8 @@ public class RobotContainer {
         Trigger coralSelectTrigger = operatorController.rightTrigger(0.5, teleopLoop);
         Trigger algaeSelectTrigger = operatorController.leftTrigger(0.5, teleopLoop);
         Trigger confirmScoreTrigger = operatorController.rightBumper(teleopLoop);
-        Trigger leftAutoAlignTrigger = driverController.leftStick();
-        Trigger rightAutoAlignTrigger = driverController.rightStick();
+        Trigger leftAutoAlignTrigger = driverController.leftTrigger();
+        Trigger rightAutoAlignTrigger = driverController.rightTrigger();
 
         if (useCompetitionBindings) {
             driverController.y().onTrue(Commands.runOnce(() -> robotDrive.resetGyro()));
