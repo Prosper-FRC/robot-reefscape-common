f66ca092ea137f964b5f53e61c540040b77715b1
Aryan Kammili <aryan.kammili@gmail.com>
2025-03-25 14:07:45 -0500

    addeds spaces - AK

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -4,15 +4,19 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class FieldConstants {
     public static final Pose2d kReefCenter = new Pose2d(4.48249, Constants.kFieldWidthMeters / 2.0, Rotation2d.fromDegrees(0.0));
     public static final double kXNetLineMeters = 7.15;
+    
     public static final Pose2d AL = new Pose2d(3.22, 4.39, Rotation2d.k180deg);
     public static final Pose2d AM = new Pose2d(3.14, 4.02, Rotation2d.k180deg);
     public static final Pose2d AR = new Pose2d(3.22, 4.08, Rotation2d.k180deg);
+
     public static final Pose2d BL = new Pose2d(4.193, 5.318, Rotation2d.fromDegrees(120.0));
     public static final Pose2d BM = new Pose2d(3.85, 5.13, Rotation2d.fromDegrees(120.0));
     public static final Pose2d BR = new Pose2d(3.927, 5.165, Rotation2d.fromDegrees(120.0));
+
     public static final Pose2d CL = new Pose2d(5.433, 4.944, Rotation2d.fromDegrees(60.0));
     public static final Pose2d CM = new Pose2d(5.15, 5.14, Rotation2d.fromDegrees(60.0));
     public static final Pose2d CR = new Pose2d(5.174, 5.105, Rotation2d.fromDegrees(60.0));
+
     public static final Pose2d DL = new Pose2d(5.76, 3.62, Rotation2d.kZero);
     public static final Pose2d DM = new Pose2d(5.76, 4, Rotation2d.kZero);
     public static final Pose2d DR = new Pose2d(5.76, 3.94, Rotation2d.kZero);
