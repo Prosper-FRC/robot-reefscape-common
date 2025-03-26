67300fbe2541fa44064c5ecaa052ef2a8b93a6ff
Aryan Kammili <aryan.kammili@gmail.com>
2025-03-26 15:24:32 -0500

    decreased my rotational kP to stop overshoot

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -48,7 +48,7 @@ public class DriveConstants {
     public static final boolean kDoExtraLogging = true;
 
     public static final PIDConstants kPPTranslationPID = new PIDConstants(1.6, 0.0, 0.0);
-    public static final PIDConstants kPPRotationPID = new PIDConstants(3.75, 0.0, 0.0);
+    public static final PIDConstants kPPRotationPID = new PIDConstants(2.5, 0.0, 0.0);
 
     ///////////////////// MODULES \\\\\\\\\\\\\\\\\\\\\\\
     /* GENERAL SWERVE MODULE CONSTANTS */
