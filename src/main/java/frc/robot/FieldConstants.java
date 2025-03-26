4e1b264503909969929b745a0f7654d15734478e
Aryan Kammili <aryan.kammili@gmail.com>
2025-03-26 15:23:31 -0500

    Updated Field Poses to home field

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -5,13 +5,13 @@ public class FieldConstants {
     public static final Pose2d kReefCenter = new Pose2d(4.48249, Constants.kFieldWidthMeters / 2.0, Rotation2d.fromDegrees(0.0));
     public static final double kXNetLineMeters = 7.15;
     
-    public static final Pose2d AL = new Pose2d(3.22, 4.39, Rotation2d.k180deg);
+    public static final Pose2d AL = new Pose2d(3.48, 4.25, Rotation2d.k180deg);
     public static final Pose2d AM = new Pose2d(3.14, 4.02, Rotation2d.k180deg);
-    public static final Pose2d AR = new Pose2d(3.22, 4.08, Rotation2d.k180deg);
+    public static final Pose2d AR = new Pose2d(3.45, 3.9, Rotation2d.k180deg);
 
-    public static final Pose2d BL = new Pose2d(4.193, 5.318, Rotation2d.fromDegrees(120.0));
+    public static final Pose2d BL = new Pose2d(4.63, 5.13, Rotation2d.fromDegrees(120.0));
     public static final Pose2d BM = new Pose2d(3.85, 5.13, Rotation2d.fromDegrees(120.0));
-    public static final Pose2d BR = new Pose2d(3.927, 5.165, Rotation2d.fromDegrees(120.0));
+    public static final Pose2d BR = new Pose2d(4.17, 4.94, Rotation2d.fromDegrees(120.0));
 
     public static final Pose2d CL = new Pose2d(5.433, 4.944, Rotation2d.fromDegrees(60.0));
     public static final Pose2d CM = new Pose2d(5.15, 5.14, Rotation2d.fromDegrees(60.0));
@@ -21,12 +21,12 @@ public class FieldConstants {
     public static final Pose2d DM = new Pose2d(5.76, 4, Rotation2d.kZero);
     public static final Pose2d DR = new Pose2d(5.76, 3.94, Rotation2d.kZero);
 
-    public static final Pose2d FL = new Pose2d(3.64, 3.13, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d FL = new Pose2d(3.55, 3.1, Rotation2d.fromDegrees(-120.0));
     public static final Pose2d FM = new Pose2d(3.86, 2.88, Rotation2d.fromDegrees(-120.0));
-    public static final Pose2d FR = new Pose2d(3.81, 2.93, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d FR = new Pose2d(3.75, 2.93, Rotation2d.fromDegrees(-120.0));
 
     public static final Pose2d EL = new Pose2d(4.78, 2.71, Rotation2d.fromDegrees(-60.0));
-    public static final Pose2d EM = new Pose2d(5.15, 2.94, Rotation2d.fromDegrees(-60.0));
+    public static final Pose2d EM = new Pose2d(5.28, 2.46, Rotation2d.fromDegrees(-60.0));
     public static final Pose2d ER = new Pose2d(5.07, 2.88, Rotation2d.fromDegrees(-60.0));
     
     public static final Pose2d B_IL = new Pose2d(1.59, 7.47, Rotation2d.fromRadians(2.20));
