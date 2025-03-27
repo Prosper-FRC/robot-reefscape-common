e3bd23a87c6512d0ed5f27b4d53e479795205f8e
AnshulBompada <ancient.flame1145@gmail.com>
2025-03-27 09:32:28 -0500

    Added logging, and set to intake rolling before comp

--- src/main/java/frc/robot/AutonCommands.java
+++ src/main/java/frc/robot/AutonCommands.java
@@ -181,7 +181,7 @@ public class AutonCommands {
                     () -> !PathPlannerAuto.currentPathName.equals(name), 
                     robotDrive.setDriveStateCommandContinued(DriveState.DRIVE_TO_CORAL).withDeadline(
                         robotDrive.waitUnitllAutoAlignFinishes()).andThen(
-                        scoreCoralCommand()), 
+                        scoreCoralCommand(), new InstantCommand(() -> mIntake.setRollerGoal(RollerGoal.kIntakeCoral))), 
                     nextAutoChecker(nextAuto))));
     }
 
@@ -220,7 +220,7 @@ public class AutonCommands {
                     () -> !PathPlannerAuto.currentPathName.equals(name), 
                     robotDrive.setDriveStateCommandContinued(DriveState.DRIVE_TO_CORAL)
                         .withDeadline(robotDrive.waitUnitllAutoAlignFinishes())
-                    .andThen(scoreCoralCommand()), 
+                    .andThen(scoreCoralCommand(), new InstantCommand(() -> mIntake.setRollerGoal(RollerGoal.kIntakeCoral))), 
                     nextAutoChecker(nextAuto))));
     }
 
@@ -235,8 +235,8 @@ public class AutonCommands {
                 nextPath(
                     name, 
                     () -> !PathPlannerAuto.currentPathName.equals(name), 
-                    robotDrive.setDriveStateCommandContinued(DriveState.DRIVE_TO_INTAKE)
-                        .withDeadline(robotDrive.waitUnitllIntakeAutoAlignFinishes()).andThen(
+                        robotDrive.setDriveStateCommandContinued(DriveState.DRIVE_TO_INTAKE)
+                            .withDeadline(robotDrive.waitUnitllIntakeAutoAlignFinishes()).andThen(
                         intakeCoralCommand().withTimeout(0.3)),
                     nextAuto)));
     }
