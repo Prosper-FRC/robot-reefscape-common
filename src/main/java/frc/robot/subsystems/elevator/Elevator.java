5004d6b0f62759f34426ab58aff18bc041da5183
AnshulBompada <ancient.flame1145@gmail.com>
2025-04-03 11:38:46 -0500

    Elevator algae setpoint was fixed.

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -30,7 +30,7 @@ public class Elevator extends SubsystemBase {
     kL205Coral(() -> 0.6),
 
     kBarge(() -> Units.inchesToMeters(60.0)),
-    kL3Algae(() -> 1.02),//kL3Algae(() -> 0.7 - 0.1),
+    kL3Algae(() -> 1.02 + Units.inchesToMeters(0.5)), //kL3Algae(() -> 0.7 - 0.1),
     kL2Algae(() -> 0.65),// kL2Algae(() -> 0.3 - 0.1),
     kProcessor(() -> 0.3 - 0.15),
     kGroundAlgae(() -> Units.inchesToMeters(8.0)),
