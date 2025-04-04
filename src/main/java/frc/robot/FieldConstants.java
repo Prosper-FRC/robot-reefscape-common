988af434a376943ba0e427e8ca1955eeb1b8a646
Alexander Phillips <86499013+AJPhillips005@users.noreply.github.com>
2025-04-04 10:17:16 -0500

    correct fr and intake red left poses

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -19,7 +19,7 @@ public class FieldConstants {
 
     public static final Pose2d FL = new Pose2d(3.51, 3.13, Rotation2d.fromDegrees(-120.0));
     public static final Pose2d FM = new Pose2d(3.86, 2.88, Rotation2d.fromDegrees(-120.0));
-    public static final Pose2d FR = new Pose2d(3.81, 2.94, Rotation2d.fromDegrees(-120.0)); // amarillo pose
+    public static final Pose2d FR = new Pose2d(3.73, 2.97, Rotation2d.fromDegrees(-120.0)); // amarillo pose
 
     public static final Pose2d EL = new Pose2d(4.76, 2.72, Rotation2d.fromDegrees(-60.0));
     public static final Pose2d EM = new Pose2d(5.15, 2.94, Rotation2d.fromDegrees(-60.0));
@@ -28,6 +28,6 @@ public class FieldConstants {
     public static final Pose2d B_IL = new Pose2d(1.45, 7.21, Rotation2d.fromRadians(2.20));
     public static final Pose2d B_IR = new Pose2d(1.16, 0.99, Rotation2d.fromRadians(-2.20));
 
-    public static final Pose2d R_IL = new Pose2d(16.03, 0.72, Rotation2d.fromRadians(2.20).plus(Rotation2d.k180deg)); // amarillo pose
+    public static final Pose2d R_IL = new Pose2d(16.03, 0.72, Rotation2d.fromRadians(2.20).plus(Rotation2d.k180deg));
     public static final Pose2d R_IR = new Pose2d(16.39, 7.06, Rotation2d.fromRadians(-2.20).plus(Rotation2d.k180deg));
 }
