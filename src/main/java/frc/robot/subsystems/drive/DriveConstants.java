ed7bd96d46c24a3e3e521d198b4de2224dc20cac
AnshulBompada <ancient.flame1145@gmail.com>
2025-04-09 17:02:32 -0500

    9492 constants

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -69,7 +69,7 @@ public class DriveConstants {
 
     public static final ModuleControlConfig kModuleControllerConfigs = RobotBase.isReal() ? 
         new ModuleControlConfig(
-            new PIDController(100.0, 0.0, 0.0), new SimpleMotorFeedforward(4.0, 0.0, 0.0),
+            new PIDController(100.0, 0.0, 0.0), new SimpleMotorFeedforward(6.5, 0.0, 1.0),
             new PIDController(20.0, 0.0, 0.5), new SimpleMotorFeedforward(0.0, 0.0, 0.0)) :
         new ModuleControlConfig(
             new PIDController(0.1, 0.0, 0.0), new SimpleMotorFeedforward(0.0, 2.36, 0.005), 
@@ -83,28 +83,28 @@ public class DriveConstants {
             11, 
             21, 
             31,
-            Rotation2d.fromRotations(0.327881).plus(Rotation2d.k180deg));
+            Rotation2d.fromRotations(-0.416992));
 
     public static final ModuleHardwareConfig kFrontRightHardware =
         new ModuleHardwareConfig(
             12, 
             22, 
             32, 
-            Rotation2d.fromRotations(0.047119).plus(Rotation2d.k180deg));
+            Rotation2d.fromRotations(0.041016));
 
     public static final ModuleHardwareConfig kBackLeftHardware =
         new ModuleHardwareConfig(
             13, 
             23, 
             33,
-            Rotation2d.fromRotations(-0.045654));
+            Rotation2d.fromRotations(-0.185303));
 
     public static final ModuleHardwareConfig kBackRightHardware =
         new ModuleHardwareConfig(
             14, 
             24, 
             34,
-            Rotation2d.fromRotations(0.273926).plus(Rotation2d.k180deg));
+            Rotation2d.fromRotations(-0.423584));
 
     public static record ModuleHardwareConfig(
         int driveID, int azimuthID, int encoderID, Rotation2d offset) {}
