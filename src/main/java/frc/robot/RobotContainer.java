690cecc06d1c7fd55b92cfcd93557e77dc3c8048
AnshulBompada <ancient.flame1145@gmail.com>
2025-04-09 16:47:48 -0500

    Another mis-click

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -372,8 +372,8 @@ public class RobotContainer {
 
             rightAutoAlignTrigger
                 .onTrue(GoalPoseChooser.setSideCommand(SIDE.RIGHT)
-                .andThen(robotDrive.setDriveStateCommandContinued(DriveState.DRIVE_TO_CORAL))
-                .onFalse(robotDrive.setDriveStateCommand(DriveState.TELEOP)));
+                .andThen(robotDrive.setDriveStateCommandContinued(DriveState.DRIVE_TO_CORAL)))
+                .onFalse(robotDrive.setDriveStateCommand(DriveState.TELEOP));
 
             driverController.a()
                 .onTrue(GoalPoseChooser.setSideCommand(SIDE.ALGAE)
