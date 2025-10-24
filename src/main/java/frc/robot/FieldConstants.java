d51f2df8fc87609b06de5e27b82ac6e27ada7973
Armann <manyata.patel11@gmail.com>
2025-10-23 19:44:54 -0500

    MP - auton working

--- src/main/java/frc/robot/FieldConstants.java
+++ src/main/java/frc/robot/FieldConstants.java
@@ -22,13 +22,13 @@ public class FieldConstants {
     public static final Pose2d DM = new Pose2d(5.76, 3.785, Rotation2d.kZero);
     public static final Pose2d DR = new Pose2d(5.76, 3.95, Rotation2d.kZero);
 
-    public static final Pose2d EL = new Pose2d(4.78, 2.7, Rotation2d.fromDegrees(-60.0));
+    public static final Pose2d EL = new Pose2d(4.753, 2.657, Rotation2d.fromDegrees(-60.0));
     public static final Pose2d EM = new Pose2d(4.915, 2.805, Rotation2d.fromDegrees(-60.0));
-    public static final Pose2d ER = new Pose2d(5.04, 2.88, Rotation2d.fromDegrees(-60.0));
+    public static final Pose2d ER = new Pose2d(5.037, 2.843, Rotation2d.fromDegrees(-60.0));
 
-    public static final Pose2d FL = new Pose2d(3.47, 3.10, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d FL = new Pose2d(3.45, 3.096, Rotation2d.fromDegrees(-120.0));
     public static final Pose2d FM = new Pose2d(3.645, 3.045, Rotation2d.fromDegrees(-120.0));
-    public static final Pose2d FR = new Pose2d(3.81, 2.94, Rotation2d.fromDegrees(-120.0));
+    public static final Pose2d FR = new Pose2d(3.763, 2.9, Rotation2d.fromDegrees(-120.0));
     
     public static final Pose2d B_IL = new Pose2d(1.45, 7.29, Rotation2d.fromRadians(2.20));
     public static final Pose2d B_IR = new Pose2d(0.88, 1.64, Rotation2d.fromRadians(-2.20));
